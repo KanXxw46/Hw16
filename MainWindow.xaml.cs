b4cf2029a49cd0bcf41d5cde5e5ee55deed31af6
Андрей Кан <1kan_andreikkkk@mail.ru>
2020-10-27 15:07:37 +0600

    Hw22

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,18 @@ namespace Hw16
         public MainWindow()
         {
             InitializeComponent();
+            List<string> styles = new List<string> { "light", "dark" };
+            styleBox.SelectionChanged += ThemeChange;
+            styleBox.ItemsSource = styles;
+            styleBox.SelectedItem = "dark";
+        }
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            string style = styleBox.SelectedItem as string;
+            var uri = new Uri(style + ".xaml", UriKind.Relative);
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
         }
     }
 }
